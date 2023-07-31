0c403194946f893c056d884e4c38e55c435b736c
oyupanqui <osmar30asis@gmail.com>
2023-07-30 22:52:41 -0500

    added initial html scheme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Alert!')
+const body = document.getElementById("content")
+
+function createMainPage() {
+    const header = document.createElement("header")
+    const main = document.createElement("div")
+    const footer = document.createElement("footer")
+
+    header.setAttribute("class", "header")
+    main.setAttribute("class", "main")
+    footer.setAttribute("class", "footer")
+
+    body.appendChild(header)
+    body.appendChild(main)
+    body.appendChild(footer)
+}
+
+createMainPage()
