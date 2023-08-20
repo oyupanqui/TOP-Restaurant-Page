d5212ea00d70f6193b58d760d48bb83f6389355b
oyupanqui <osmar30asis@gmail.com>
2023-08-20 00:28:13 -0500

    modified initialize js

--- src/home.js
+++ src/home.js
@@ -1,27 +1,8 @@
 const createHome = () => {
-    const body = document.getElementById("content")
-
-    const header = document.createElement("div")
-    header.setAttribute("class", "header")
-    header.textContent = 'Welcome to my Restaurant!'
-    body.appendChild(header)
-
-    const main = document.createElement("div")
-    main.setAttribute("class", "main")
-
-    const image = document.createElement("img")
-    image.src = 'images/main-logo.jpeg'
-    image.height = '300'
-
-    main.appendChild(image)
-    body.appendChild(main)
-
-    const footer = document.createElement("div")
-    footer.setAttribute("class", "footer")
-    const ty = document.createElement("p")
-    ty.textContent = "Thanks for visiting!"
-    footer.appendChild(ty)
-    body.appendChild(footer)
+    const home = document.createElement("div")
+    header.setAttribute("class", "home")
+    
+    
 }
 
 export default createHome
--- src/initialize.js
+++ src/initialize.js
@@ -1,7 +1,47 @@
-import createHome from './home'
+function createHeader() {
+    const header = document.createElement("header")
+    header.setAttribute("class", "header")
+
+    const homeButton = document.createElement("button")
+    homeButton.setAttribute("class", "btn-nav home")
+    homeButton.textContent = "Home"
+    homeButton.addEventListener("click", (e) => {
+        //loadHome()
+
+    })
+
+    const menuButton = document.createElement("button")
+    menuButton.setAttribute("class", "btn-nav menu")
+    menuButton.textContent = "Menu"
+    menuButton.addEventListener("click", (e) => {
+        //loadMenu()
+    })
+
+    const contactButton = document.createElement("button")
+    contactButton.setAttribute("class", "btn-nav contact")
+    contactButton.textContent = "Contact"
+    contactButton.addEventListener("click", (e) => {
+        //loadMenu()
+    })
+
+    header.appendChild(homeButton)
+    header.appendChild(menuButton)
+    header.appendChild(contactButton)
+
+    return header
+}
+
+function createMain() {
+
+}
+
+function createFooter() {
+    
+}
 
 function initialize() {
-    createHome()
+    const content = document.getElementById("content")
+    content.appendChild(createHeader())
 }
 
 export default initialize
